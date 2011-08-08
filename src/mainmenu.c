2aa9f938355d9221af9c0caa6baed666b1184ce6
Steve Fryatt <info@stevefryatt.org.uk>
2011-08-08 22:16:58 +0000

    Tidy formatting.

--- src/mainmenu.c
+++ src/mainmenu.c
@@ -228,6 +228,21 @@ static void amenu_close(void)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* ==================================================================================================================
  * Account menu -- List of accounts and headings to select from
  */
@@ -235,8 +250,8 @@ static void amenu_close(void)
 
 /* ------------------------------------------------------------------------------------------------------------------ */
 
-void open_account_menu (file_data *file, enum account_menu_type type, int line,
-                        wimp_w window, wimp_i icon_i, wimp_i icon_n, wimp_i icon_r, wimp_pointer *pointer)
+void open_account_menu(file_data *file, enum account_menu_type type, int line,
+		wimp_w window, wimp_i icon_i, wimp_i icon_n, wimp_i icon_r, wimp_pointer *pointer)
 {
 	wimp_menu		*menu;
 
@@ -249,112 +264,99 @@ void open_account_menu (file_data *file, enum account_menu_type type, int line,
 
 	amenu_open(menu, pointer, NULL, account_menu_submenu_message, decode_account_menu, account_menu_closed_message);
 
-  main_menu_file = file;
-  main_menu_line = line;
-  main_menu_column = type;
+	main_menu_file = file;
+	main_menu_line = line;
+	main_menu_column = type;
 
-  account_menu_window = window;
-  account_menu_name_icon = icon_n;
-  account_menu_ident_icon = icon_i;
-  account_menu_rec_icon = icon_r;
+	account_menu_window = window;
+	account_menu_name_icon = icon_n;
+	account_menu_ident_icon = icon_i;
+	account_menu_rec_icon = icon_r;
 }
 
 /* ------------------------------------------------------------------------------------------------------------------ */
 
 static void decode_account_menu(wimp_selection *selection)
 {
-  int i, column, account;
+	int i, column, account;
 
-  if (account_menu_window == NULL)
-  {
-    /* If the window is NULL, then the menu was opened over the transaction window. */
+	if (account_menu_window == NULL) {
+		/* If the window is NULL, then the menu was opened over the transaction window. */
 
-    /* Check that the line is in the range of transactions.  If not, add blank transactions to the file until it is.
-     * This really ought to be in edit.c!
-     */
+		/* Check that the line is in the range of transactions.  If not, add blank transactions to the file until it is.
+		 * This really ought to be in edit.c!
+		 */
 
-    if (main_menu_line >= main_menu_file->trans_count && selection->items[0] != -1)
-    {
-      for (i=main_menu_file->trans_count; i<=main_menu_line; i++)
-      {
-        add_raw_transaction (main_menu_file,
-                             NULL_DATE, NULL_ACCOUNT, NULL_ACCOUNT, NULL_TRANS_FLAGS, NULL_CURRENCY, "", "");
-      }
-    }
+		if (main_menu_line >= main_menu_file->trans_count && selection->items[0] != -1)
+			for (i=main_menu_file->trans_count; i<=main_menu_line; i++)
+				add_raw_transaction(main_menu_file, NULL_DATE, NULL_ACCOUNT, NULL_ACCOUNT, NULL_TRANS_FLAGS, NULL_CURRENCY, "", "");
 
-    /* Again check that the transaction is in range.  If it isn't, the additions failed.
-     *
-     * Then change the transaction as instructed.
-     */
+		/* Again check that the transaction is in range.  If it isn't, the additions failed.
+		 *
+		 * Then change the transaction as instructed.
+		 */
 
-    if (main_menu_line < main_menu_file->trans_count && selection->items[1] != -1)
-    {
-      switch (main_menu_column)
-      {
-        case ACCOUNT_MENU_FROM:
-          column = EDIT_ICON_FROM;
-          break;
+		if (main_menu_line < main_menu_file->trans_count && selection->items[1] != -1) {
+			switch (main_menu_column) {
+			case ACCOUNT_MENU_FROM:
+				column = EDIT_ICON_FROM;
+				break;
 
-        case ACCOUNT_MENU_TO:
-          column = EDIT_ICON_TO;
-          break;
+			case ACCOUNT_MENU_TO:
+				column = EDIT_ICON_TO;
+				break;
 
-        default:
-          column = -1;
-          break;
-      }
+			default:
+				column = -1;
+				break;
+			}
 
-      edit_change_transaction_account (main_menu_file, main_menu_file->transactions[main_menu_line].sort_index, column,
-                                       account_complete_menu_decode(selection));
-    }
-  }
-  else
-  {
-    /* If the window is not NULL, the menu was opened over a dialogue box. */
+			edit_change_transaction_account(main_menu_file, main_menu_file->transactions[main_menu_line].sort_index, column,
+					account_complete_menu_decode(selection));
+		}
+	} else {
+		/* If the window is not NULL, the menu was opened over a dialogue box. */
 
-    if (selection->items[1] != -1)
-    {
-      account = account_complete_menu_decode(selection);
+		if (selection->items[1] != -1) {
+			account = account_complete_menu_decode(selection);
 
-      fill_account_field (main_menu_file, account, !(main_menu_file->accounts[account].type & ACCOUNT_FULL),
-                          account_menu_window, account_menu_ident_icon, account_menu_name_icon, account_menu_rec_icon);
+			fill_account_field(main_menu_file, account, !(main_menu_file->accounts[account].type & ACCOUNT_FULL),
+					account_menu_window, account_menu_ident_icon, account_menu_name_icon, account_menu_rec_icon);
 
-      wimp_set_icon_state (account_menu_window, account_menu_ident_icon, 0, 0);
-      wimp_set_icon_state (account_menu_window, account_menu_name_icon, 0, 0);
-      wimp_set_icon_state (account_menu_window, account_menu_rec_icon, 0, 0);
+			wimp_set_icon_state(account_menu_window, account_menu_ident_icon, 0, 0);
+			wimp_set_icon_state(account_menu_window, account_menu_name_icon, 0, 0);
+			wimp_set_icon_state(account_menu_window, account_menu_rec_icon, 0, 0);
 
-      icons_replace_caret_in_window (account_menu_window);
-    }
-  }
+			icons_replace_caret_in_window(account_menu_window);
+		}
+	}
 }
 
 /* ------------------------------------------------------------------------------------------------------------------ */
 
 static void account_menu_submenu_message(wimp_message_menu_warning *submenu)
 {
-  wimp_menu *menu_block;
+	wimp_menu *menu_block;
 
-  if (submenu->selection.items[1] == -1)
-  {
-    menu_block = account_complete_submenu_build(submenu);
-    wimp_create_sub_menu(menu_block, submenu->pos.x, submenu->pos.y);
-  }
+	if (submenu->selection.items[0] == -1 || submenu->selection.items[1] != -1)
+		return;
+
+	menu_block = account_complete_submenu_build(submenu);
+	wimp_create_sub_menu(menu_block, submenu->pos.x, submenu->pos.y);
 }
 
 /* ------------------------------------------------------------------------------------------------------------------ */
 
 static void account_menu_closed_message (void)
 {
-  extern global_windows windows;
+	extern global_windows windows;
 
-  if (account_menu_window == windows.enter_acc)
-  {
-    close_account_lookup_account_menu ();
-  }
+	if (account_menu_window == windows.enter_acc)
+		close_account_lookup_account_menu();
 
-  account_complete_menu_destroy();
+	account_complete_menu_destroy();
 
-  templates_set_menu_token("");
+	templates_set_menu_token("");
 }
 
 /* ------------------------------------------------------------------------------------------------------------------ */
@@ -393,41 +395,28 @@ static void decode_date_menu(wimp_selection *selection)
 		return;
 
 
-  if (main_menu_file != NULL)
-  {
-    /* Check that the line is in the range of transactions.  If not, add blank transactions to the file until it is.
-     * This really ought to be in edit.c!
-     */
+	if (main_menu_file == NULL)
+		return;
 
-    if (main_menu_line >= main_menu_file->trans_count && selection->items[0] != -1)
-    {
-      for (i=main_menu_file->trans_count; i<=main_menu_line; i++)
-      {
-        add_raw_transaction (main_menu_file,
-                             NULL_DATE, NULL_ACCOUNT, NULL_ACCOUNT, NULL_TRANS_FLAGS, NULL_CURRENCY, "", "");
-      }
-    }
+	/* Check that the line is in the range of transactions.  If not, add blank transactions to the file until it is.
+	 * This really ought to be in edit.c!
+	 */
 
-    /* Again check that the transaction is in range.  If it isn't, the additions failed.
-     *
-     * Then change the transaction as instructed.
-     */
+	if (main_menu_line >= main_menu_file->trans_count && selection->items[0] != -1)
+		for (i=main_menu_file->trans_count; i<=main_menu_line; i++)
+			add_raw_transaction(main_menu_file, NULL_DATE, NULL_ACCOUNT, NULL_ACCOUNT, NULL_TRANS_FLAGS, NULL_CURRENCY, "", "");
 
-    if (main_menu_line < main_menu_file->trans_count && selection->items[0] != -1)
-    {
-      if (preset_complete_menu_decode(selection) == NULL_PRESET)
-      {
-        edit_change_transaction_date (main_menu_file, main_menu_file->transactions[main_menu_line].sort_index,
-                                      get_current_date ());
+	/* Again check that the transaction is in range.  If it isn't, the additions failed.
+	 *
+	 * Then change the transaction as instructed.
+	 */
 
-      }
-      else
-      {
-        insert_transaction_preset_full (main_menu_file, main_menu_file->transactions[main_menu_line].sort_index,
-                                        preset_complete_menu_decode(selection));
-      }
-    }
-  }
+	if (main_menu_line < main_menu_file->trans_count && selection->items[0] != -1) {
+		if (preset_complete_menu_decode(selection) == NULL_PRESET)
+			edit_change_transaction_date(main_menu_file, main_menu_file->transactions[main_menu_line].sort_index, get_current_date());
+		else
+			insert_transaction_preset_full(main_menu_file, main_menu_file->transactions[main_menu_line].sort_index, preset_complete_menu_decode(selection));
+	}
 }
 
 static void date_menu_closed_message(void)
@@ -463,10 +452,9 @@ void open_refdesc_menu (file_data *file, int menu_type, int line, wimp_pointer *
 		break;
 	}
 
-  main_menu_file = file;
-  main_menu_line = line;
-  refdesc_menu_type = menu_type;
-
+	main_menu_file = file;
+	main_menu_line = line;
+	refdesc_menu_type = menu_type;
 
 	amenu_open(menu, pointer, refdesc_menu_prepare, NULL, decode_refdesc_menu, refdesc_menu_closed_message);
 }
@@ -482,57 +470,40 @@ static void refdesc_menu_prepare(void)
 
 static void decode_refdesc_menu (wimp_selection *selection)
 {
-  int  i;
-  char *field, cheque_buffer[REF_FIELD_LEN];
+	int		i;
+	char		*field, cheque_buffer[REF_FIELD_LEN];
 
+	if (main_menu_file == NULL)
+		return;
 
-  if (main_menu_file != NULL)
-  {
-    /* Check that the line is in the range of transactions.  If not, add blank transactions to the file until it is.
-     * This really ought to be in edit.c!
-     */
+	/* Check that the line is in the range of transactions.  If not, add blank transactions to the file until it is.
+	 * This really ought to be in edit.c!
+	 */
 
-    if (main_menu_line >= main_menu_file->trans_count && selection->items[0] != -1)
-    {
-      for (i=main_menu_file->trans_count; i<=main_menu_line; i++)
-      {
-        add_raw_transaction (main_menu_file,
-                             NULL_DATE, NULL_ACCOUNT, NULL_ACCOUNT, NULL_TRANS_FLAGS, NULL_CURRENCY, "", "");
-      }
-    }
+	if (main_menu_line >= main_menu_file->trans_count && selection->items[0] != -1)
+		for (i=main_menu_file->trans_count; i<=main_menu_line; i++)
+			add_raw_transaction(main_menu_file, NULL_DATE, NULL_ACCOUNT, NULL_ACCOUNT, NULL_TRANS_FLAGS, NULL_CURRENCY, "", "");
 
-    /* Again check that the transaction is in range.  If it isn't, the additions failed.
-     *
-     * Then change the transaction as instructed.
-     */
+	/* Again check that the transaction is in range.  If it isn't, the additions failed.
+	 *
+	 * Then change the transaction as instructed.
+	 */
 
-    if (main_menu_line < main_menu_file->trans_count && selection->items[0] != -1)
-    {
-    	field = transact_complete_menu_decode(selection);
+	if (main_menu_line < main_menu_file->trans_count && selection->items[0] != -1) {
+		field = transact_complete_menu_decode(selection);
 
-      if (refdesc_menu_type == REFDESC_MENU_REFERENCE && field == NULL)
-      {
-        get_next_cheque_number (main_menu_file,
-                  main_menu_file->transactions[main_menu_file->transactions[main_menu_line].sort_index].from,
-                  main_menu_file->transactions[main_menu_file->transactions[main_menu_line].sort_index].to,
-                  1, cheque_buffer);
-        edit_change_transaction_refdesc (main_menu_file, main_menu_file->transactions[main_menu_line].sort_index,
-                                        EDIT_ICON_REF, cheque_buffer);
-      }
-      else if (refdesc_menu_type == REFDESC_MENU_REFERENCE && field != NULL)
-      {
-        edit_change_transaction_refdesc (main_menu_file, main_menu_file->transactions[main_menu_line].sort_index,
-                                        EDIT_ICON_REF, field);
-
-      }
-      else if (refdesc_menu_type == REFDESC_MENU_DESCRIPTION && field != NULL)
-      {
-        edit_change_transaction_refdesc (main_menu_file, main_menu_file->transactions[main_menu_line].sort_index,
-                                        EDIT_ICON_DESCRIPT, field);
-
-      }
-    }
-  }
+		if (refdesc_menu_type == REFDESC_MENU_REFERENCE && field == NULL) {
+			get_next_cheque_number(main_menu_file,
+					main_menu_file->transactions[main_menu_file->transactions[main_menu_line].sort_index].from,
+					main_menu_file->transactions[main_menu_file->transactions[main_menu_line].sort_index].to,
+					1, cheque_buffer);
+			edit_change_transaction_refdesc(main_menu_file, main_menu_file->transactions[main_menu_line].sort_index, EDIT_ICON_REF, cheque_buffer);
+		} else if (refdesc_menu_type == REFDESC_MENU_REFERENCE && field != NULL) {
+			edit_change_transaction_refdesc(main_menu_file, main_menu_file->transactions[main_menu_line].sort_index, EDIT_ICON_REF, field);
+		} else if (refdesc_menu_type == REFDESC_MENU_DESCRIPTION && field != NULL) {
+			edit_change_transaction_refdesc(main_menu_file, main_menu_file->transactions[main_menu_line].sort_index, EDIT_ICON_DESCRIPT, field);
+		}
+	}
 }
 
 /* ------------------------------------------------------------------------------------------------------------------ */

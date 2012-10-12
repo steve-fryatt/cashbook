7ec13ca0b96afa40d12bf45ac0c16281e3aaefb8
Steve Fryatt <info@stevefryatt.org.uk>
2012-10-12 21:46:14 +0000

    Convert licensing to EUPL.

--- src/edit.c
+++ src/edit.c
@@ -1,6 +1,30 @@
-/* CashBook - edit.c
+/* Copyright 2003-2012, Stephen Fryatt
  *
- * (C) Stephen Fryatt, 2003
+ * This file is part of CashBook:
+ *
+ *   http://www.stevefryatt.org.uk/software/
+ *
+ * Licensed under the EUPL, Version 1.1 only (the "Licence");
+ * You may not use this work except in compliance with the
+ * Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ *
+ *   http://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+
+/**
+ * \file: edit.c
+ *
+ * Transaction editing implementation.
  */
 
 /* ANSI C header files */
--- src/global.h
+++ src/global.h
@@ -1,10 +1,34 @@
-/* CashBook - global.h
+/* Copyright 2003-2012, Stephen Fryatt
  *
- * (c) Stephen Fryatt, 2003
+ * This file is part of CashBook:
+ *
+ *   http://www.stevefryatt.org.uk/software/
+ *
+ * Licensed under the EUPL, Version 1.1 only (the "Licence");
+ * You may not use this work except in compliance with the
+ * Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ *
+ *   http://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
  */
 
-#ifndef _ACCOUNTS_GLOBAL
-#define _ACCOUNTS_GLOBAL
+/**
+ * \file: global.c
+ *
+ * Legacy global datastructure support.
+ */
+
+#ifndef CASHBOOK_GLOBAL
+#define CASHBOOK_GLOBAL
 
 #include "oslib/wimp.h"
 

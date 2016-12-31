6268ce8ffc5288abe4b11c505e36632ffcd6f328
Steve Fryatt <info@stevefryatt.org.uk>
2016-12-31 20:51:49 +0000

    Start to add more placeholders for the interest code.

--- src/interest.c
+++ src/interest.c
@@ -129,3 +129,41 @@ void interest_delete_instance(struct interest_block *instance)
 
 	heap_free(instance);
 }
+
+
+/**
+ * Return an interest rate for a given account on a given date. Returns
+ * NULL_RATE on failure.
+ *
+ * \param *instance		The interest rate module instance to use.
+ * \param account		The account to return an interest rate for.
+ * \param date			The date to return the rate for.
+ * \return			The interest rate on the date in question.
+ */
+
+rate_t interest_get_current_rate(struct interest_block *instance, acct_t account, date_t date)
+{
+	return NULL_RATE;
+}
+
+
+/**
+ * Convert an interest rate into a string, placing the result into a
+ * supplied buffer.
+ *
+ * \param rate			The rate to be converted.
+ * \param *buffer		Pointer to a buffer to take the conversion.
+ * \param length		The size of the supplied buffer, in bytes.
+ * \return			A pointer to the supplied buffer, or NULL if
+ *				the buffer's details were invalid.
+ */
+
+char *interest_convert_to_string(rate_t rate, char *buffer, size_t length)
+{
+	if (buffer == NULL || length == 0)
+		return NULL;
+
+	*buffer = '\0';
+
+	return buffer;
+}
--- src/interest.h
+++ src/interest.h
@@ -30,9 +30,28 @@
 #ifndef CASHBOOK_INTEREST
 #define CASHBOOK_INTEREST
 
+/**
+ * An interest rate representation.
+ */
+
+typedef int rate_t;
+
+/**
+ * Invalid or missing interest rate.
+ */
+
+#define NULL_RATE ((rate_t) -1)
+
+/**
+ * An interest rate management instance.
+ */
+
 struct interest_block;
 
 #include "global.h"
+
+#include "account.h"
+#include "date.h"
 
 /**
  * Initialise the transaction system.
@@ -58,5 +77,32 @@ struct interest_block *interest_create_instance(struct file_block *file);
  */
 
 void interest_delete_instance(struct interest_block *instance);
+
+
+/**
+ * Return an interest rate for a given account on a given date. Returns
+ * NULL_RATE on failure.
+ *
+ * \param *instance		The interest rate module instance to use.
+ * \param account		The account to return an interest rate for.
+ * \param date			The date to return the rate for.
+ * \return			The interest rate on the date in question.
+ */
+
+rate_t interest_get_current_rate(struct interest_block *instance, acct_t account, date_t date);
+
+
+/**
+ * Convert an interest rate into a string, placing the result into a
+ * supplied buffer.
+ *
+ * \param rate			The rate to be converted.
+ * \param *buffer		Pointer to a buffer to take the conversion.
+ * \param length		The size of the supplied buffer, in bytes.
+ * \return			A pointer to the supplied buffer, or NULL if
+ *				the buffer's details were invalid.
+ */
+
+char *interest_convert_to_string(rate_t rate, char *buffer, size_t length);
 
 #endif

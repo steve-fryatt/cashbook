d9071970e8ed811790506443e70c548c09d80ed5
Steve Fryatt <info@stevefryatt.org.uk>
2026-08-01 11:59:40 +0100

    Cherry-pick unit test support from development
    This adds support using Unity 2.7.0. No tests are currently implemented.

--- src/main.c
+++ src/main.c
@@ -131,6 +131,8 @@ static date_t			main_last_update_date = NULL_DATE;
 wimp_t				main_task_handle;
 osbool				main_quit_flag = FALSE;
 
+#ifndef UNIT_TESTING
+
 /**
  * Main code entry point.
  */
@@ -148,6 +150,7 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+#endif
 
 /**
  * Wimp Poll loop.
